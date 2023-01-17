718745a1c96ad1034416b0abdce000a2e83ea8bc
afshin <afshin89hatampour@gmail.com>
2023-01-17 23:30:12 +0330

    login page

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,5 +1,9 @@
-import {Navigate, Outlet} from "react-router-dom";
+import {Link, Navigate, Outlet} from "react-router-dom";
 import {useStateContext} from "../contexts/ContextProvider.jsx";
+
+const onLogout = (ev) => {
+    ev.preventDefault();
+}
 
 export default function DefaultLayout() {
     const {user, token} = useStateContext();
@@ -8,8 +12,23 @@ export default function DefaultLayout() {
     }
 
     return (
-        <>
-            <Outlet/>
-        </>
+        <div id="defaultLayout">
+            <aside>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/users">Users</Link>
+            </aside>
+            <div className="content">
+                <header>
+                    <div>Header</div>
+                    <div>
+                        {user.name}
+                        <a href="#" onClick={onLogout} className="btn btn-logout">Logout</a>
+                    </div>
+                </header>
+                <main>
+                    <Outlet/>
+                </main>
+            </div>
+        </div>
     );
 }
